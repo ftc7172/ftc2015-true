89f1cc3f44fe444b8c64f657af0f14925fd052d7
FTC7172 <github@techdifficulties.org>
2016-02-10 15:29:07 -0800

    Initial version of autonomous mode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OhAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OhAuto.java
@@ -1,9 +1,9 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.GyroSensor;
@@ -18,8 +18,10 @@ public class OhAuto extends LinearOpMode {
 
     double tiltTarget = 0;
     double panTarget = 0;
+    double traveled = 0;
+    double pastPos = 0;
     GyroSensor gyro;
-
+    DcMotor extendMotor;
     Servo intakeServo;
 
     Toggle intakeToggle;
@@ -29,6 +31,8 @@ public class OhAuto extends LinearOpMode {
     DcMotor rf;
     DcMotor rb;
 
+    OpticalDistanceSensor opD;
+
 
     public OhAuto() {
     }
@@ -37,49 +41,120 @@ public class OhAuto extends LinearOpMode {
         public void runOpMode() throws InterruptedException{
         tiltMotor = hardwareMap.dcMotor.get("tilt");
         tiltMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        tiltMotor.setDirection(DcMotor.Direction.REVERSE);
         panMotor = hardwareMap.dcMotor.get("pan");
         gyro = hardwareMap.gyroSensor.get("gyro");
         lf = hardwareMap.dcMotor.get("lf");
+        lf.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         lb = hardwareMap.dcMotor.get("lb");
         rf = hardwareMap.dcMotor.get("rf");
         rb = hardwareMap.dcMotor.get("rb");
+        extendMotor = hardwareMap.dcMotor.get("extend");
+        extendMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+       // opD = hardwareMap.opticalDistanceSensor.get("opdist");
         rf.setDirection(DcMotor.Direction.REVERSE);
+        rf.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         rb.setDirection(DcMotor.Direction.REVERSE);
         gyro.calibrate();
+        while(gyro.isCalibrating()){
+            waitOneFullHardwareCycle();
+        }
         waitForStart();
-        drive(30, .15, 0);
+        extendMotor.setPowerFloat();
+        drive(0, -.2, 4200);
+        drive(-45, -.2, 7500);
+        drive(-80, -.2, 500);
+        climberScore();
+        /*
+        drive(0, .15, 50)
+        while(opD.getLightDetected() < "Red"){
+            drive(-30, .15, 10);
+        }
+        while(opD.getLightDetected < "white"){
+            drive(0, .15, 10);
+        }
+        drive(-90, .15, 10);
+        lineFollow();
+        scoreClimbers();
+        */
+
         }
 
     public void drive(double targetAngle, double throttle, double distance)throws InterruptedException{
-        while(true) {
-            double error = (gyro.getHeading () > 180 ? gyro.getHeading() - 360 : gyro.getHeading() - targetAngle) / (50.0);
+
+        int startPos = rf.getCurrentPosition();
+        while (Math.abs(rf.getCurrentPosition() - startPos) < distance) {
+            double error = ((gyro.getHeading() > 180 ? gyro.getHeading() - 360 : gyro.getHeading()) - targetAngle) / (50.0);
             throttle = Range.clip(throttle, -1, 1);
-
-
             double right = throttle + error;
             double left = throttle - error;
 
-            right = Range.clip(right, -1, 1);
-            left = Range.clip(left, -1, 1);
+            right = Range.clip(right, -1, 0);
+            left = Range.clip(left, -1, 0);
+            rf.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            rb.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            lf.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            lb.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
             rf.setPower(right);
             rb.setPower(right);
             lf.setPower(left);
             lb.setPower(left);
 
-            waitOneFullHardwareCycle();
+            telemetry.addData("rfpos", rf.getCurrentPosition());
+
             telemetry.addData("heading", gyro.getHeading());
             telemetry.addData("error", error);
+            //telemetry.addData("color", opD.getLightDetected());
+            waitOneFullHardwareCycle();
         }
+        rf.setPower(0);
+        rb.setPower(0);
+        lf.setPower(0);
+        lb.setPower(0);
+        waitOneFullHardwareCycle();
     }
 
 
-    public void climberScore(){
-
+    public void climberScore() throws InterruptedException{
+        tiltArm(1700);
+       // tiltArm(100);
     }
 
-    public void lineFollow(){
-
+    public void tiltArm(double t) throws InterruptedException{
+        int PID_RANGE = 50;
+        tiltTarget = t;
+        telemetry.addData("tiltTarget", tiltTarget);
+        //telemetry.addData("tiltPos", tiltPos);
+        while(true) {
+            double tiltPos = tiltMotor.getCurrentPosition();
+            if (tiltPos > tiltTarget) {
+                break;
+            }
+            tiltMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+            tiltMotor.setPower(.2);
+            waitOneFullHardwareCycle();
+        }
+        tiltMotor.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        tiltMotor.setPower(0);
+        waitOneFullHardwareCycle();
     }
 
+ /*   public void lineFollow(double lnColor) throws InterruptedException{
+        double i = 0;
+        while(true){
+            double light = opD.getLightDetected();
+            while(light < lnColor) {
+                i = 0;
+                drive(0, .15, 10);
+                light = opD.getLightDetected();
+            }
+            i+= .1;
+            double d = Math.sin(i);
+            drive(d, .15, 10);
+        }
+
+    }
+*/
 
 }
