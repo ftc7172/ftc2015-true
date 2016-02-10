064e8c81bb1c66478e684ea09343706ceec1d6f9
FTC7172 <github@techdifficulties.org>
2016-02-10 15:33:57 -0800

    Add count of toggles

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Toggle.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Toggle.java
@@ -4,16 +4,22 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by robotics on 1/5/2016.
  */
 public class Toggle {
-    boolean onoff = false;
+    public int count = 0;
+    public boolean onoff = false;
     boolean buttonLast = false;
 
+    public boolean onPress(boolean buttonNow) {
+        if (buttonLast && !buttonNow) { count++; onoff = !onoff; }
+        buttonLast = buttonNow;
+        return onoff;
+    }
+
     public boolean onRelease(boolean buttonNow) {
-        if (buttonLast && !buttonNow) onoff = !onoff;
+        if (!buttonLast && buttonNow) { count++; onoff = !onoff; }
         buttonLast = buttonNow;
         return onoff;
     }
 
     public boolean isOnoff() { return onoff; }
 }
-
 
